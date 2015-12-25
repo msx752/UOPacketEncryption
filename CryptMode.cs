02d4d3e7118a66c1bad483e9fc203d6694fb6cd8
Mustafa Salih ASLIM <mustys@users.noreply.github.com>
2015-12-26 00:33:45 +0200

    Ultima Online game packet encryption class
    Ultima Online game packet encryption class

---
+++ CryptMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.PacketEncryption
+{
+    public enum CryptMode
+    {
+        CRYPT_LOGIN,
+        CRYPT_GAME,
+        CRYPT_NONE
+    }
+}
---
+++ Encrypter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltimaXNA.PacketEncryption
+{
+    public static class Encrypter
+    {
+        public static UOCrypt uoc = new UOCrypt();
+
+        private static uint LOGINKEY1_V2_0_3 = 0x2DBBB7CD;
+        private static uint LOGINKEY2_V2_0_3 = 0xA3C95E7F;
+        public static CryptMode m_crypt_mode = CryptMode.CRYPT_NONE;
+
+        //use this if packet first byte == 0x80  (ONCE)
+        public static void set_login_encryption()
+        {
+            m_crypt_mode = CryptMode.CRYPT_LOGIN;
+            uoc.LogCrypt.init(LOGINKEY1_V2_0_3, LOGINKEY2_V2_0_3);
+        }
+
+        //use this if packet first byte == 0x91  (ONCE)
+        public static void set_game_encryption()
+        {
+            m_crypt_mode = CryptMode.CRYPT_GAME;
+            uoc.GamCrypt.init();
+        }
+
+        //use this if packet first byte == 0xC0  (ONCE)
+        public static void SEED(byte[] PACKET)
+        {
+            uint[] pseed = new uint[PACKET.Length];
+            for (int i = 0; i < PACKET.Length; i++)
+            {
+                pseed[i] = Convert.ToUInt32(PACKET[i]);
+            }
+            uoc.LogCrypt.SEEDK = pseed;
+        }
+    }
+}
